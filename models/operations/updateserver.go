0808da75e4dadbae2c0daa065f3f2feb767e3a32
Leandro Parazito <leandroh@gmail.com>
2025-07-16 14:28:57 -0300

    fix(struct): remove default from server update request

--- models/operations/updateserver.go
+++ models/operations/updateserver.go
@@ -161,7 +161,7 @@ func (e *UpdateServerServersRequestApplicationJSONBilling) UnmarshalJSON(data []
 
 type UpdateServerServersRequestApplicationJSONAttributes struct {
 	Hostname *string                                           `default:"new-hostname" json:"hostname"`
-	Billing  *UpdateServerServersRequestApplicationJSONBilling `default:"monthly" json:"billing"`
+	Billing  *UpdateServerServersRequestApplicationJSONBilling `json:"billing",omitempty`
 	Tags     []string                                          `json:"tags,omitempty"`
 	Project  *string                                           `json:"project,omitempty"`
 }
